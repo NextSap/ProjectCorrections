afc6fe715a12e7be8041d4ecf51c20c2af6da535
Mamak_ <pro.mamak4@gmail.com>
2021-02-25 20:22:48 +0100

    :rocket: NextSap

--- project3/Mamak_/main.py
+++ project3/Mamak_/main.py
@@ -2,148 +2,158 @@ import random
 
 
 def get_random_word():
-    words = []
-    with open('words.txt', 'r') as file:
-        for word in file:
-            words.append(word.replace('\n', ''))
-    selection = random.choice(words)
-    return selection  # OK
+	words = []
+	with open('words.txt', 'r') as file:
+    	for word in file:
+        	words.append(word.replace('\n', ''))
+	selection = random.choice(words)
+	return selection  # OK
 
 
 def get_result(word, userinput):
-    result = bool(False)
-    array = list(word)
-    save = []
-    tool = 0
-    for loop in array:
-        if userinput == loop:
-            result = bool(True)
-            save.append(tool)
-        tool += 1
-    print(result, save)
-    return result, save  # OK
+	result = bool(False)
+	array = list(word)
+	save = []
+	tool = 0
+	for loop in array:
+    	if userinput == loop:
+        	result = bool(True)
+        	save.append(tool)
+    	tool += 1
+	print(result, save)
+	return result, save  # OK
 
 
 def get_draw(draw):
-    tab = [
-        """
-           +-------+
-           |
-           |
-           |
-           |
-           |
-        ==============
-        """,
-        """
-           +-------+
-           |   	|
-           |   	O
-           |
-           |
-           |
-        ==============
-        """
-        ,
-        """
-           +-------+
-           |   	|
-           |   	O
-           |   	|
-           |
-           |
-        ==============
-        """,
-        """
-           +-------+
-           |   	|
-           |   	O
-           |  	-|
-           |
-           |
-        ==============
-        """,
-        """
-           +-------+
-           |   	|
-           |   	O
-           |  	-|-
-           |
-           |
-        ==============
-        """,
-        """
-           +-------+
-           |   	|
-           |   	O
-           |  	-|-
-           |  	|
-           |
-        ==============
-        """,
-        """
-           +-------+
-           |   	|
-           |   	O
-           |  	-|-
-           |  	| |
-           |
-        ==============
-        """
-    ]
-    return tab[draw]
+	tab = [
+    	"""
+       	+-------+
+       	|
+       	|
+       	|
+       	|
+       	|
+    	==============
+    	""",
+    	"""
+       	+-------+
+       	|  	 |
+       	|  	 O
+       	|
+       	|
+       	|
+    	==============
+    	"""
+    	,
+    	"""
+       	+-------+
+       	|  	 |
+       	|  	 O
+       	|  	 |
+       	|
+       	|
+    	==============
+    	""",
+    	"""
+       	+-------+
+       	|  	 |
+       	|  	 O
+       	| 	 -|
+       	|
+       	|
+    	==============
+    	""",
+    	"""
+       	+-------+
+       	|  	 |
+       	|  	 O
+       	| 	 -|-
+       	|
+       	|
+    	==============
+    	""",
+    	"""
+       	+-------+
+       	|  	 |
+       	|  	 O
+       	| 	 -|-
+       	| 	 |
+       	|
+    	==============
+    	""",
+    	"""
+       	+-------+
+       	|  	 |
+       	|  	 O
+       	| 	 -|-
+       	| 	 | |
+       	|
+    	==============
+    	"""
+	]
+	return tab[draw]
 
 
 def cheating_check(user_responses, user_input, word):
-    for check in user_responses:
-        if user_input == check:
-            result = get_result(word, user_input)[0]
-            if result:
-                print("Vous avez déjà rentré cette lettre, de plus, cette réponse était correcte.")
-            else:
-                print("Vous avez déjà rentré cette lettre, de plus, cette réponse était incorrecte.")
-            cheating = bool(True)
-        else:
-            cheating = bool(False)
-        return cheating
+	for check in user_responses:
+    	if user_input == check:
+        	result = get_result(word, user_input)[0]
+        	if result:
+            	print("Vous avez déjà rentré cette lettre, de plus, cette réponse était correcte.")
+        	else:
+            	print("Vous avez déjà rentré cette lettre, de plus, cette réponse était incorrecte.")
+        	cheating = bool(True)
+    	else:
+        	cheating = bool(False)
+    	return cheating
 
 
 def ask_for_a_letter(user_responses, word):
-    user_input = input("Veuillez entrer une lettre: ").upper()
+	user_input = input("Veuillez entrer une lettre: ").upper()
 
-    cheating = cheating_check(user_responses, user_input, word)
-    if cheating == bool(False):
-        user_responses.append(user_input)
-    return get_result(word, user_input)[0], user_input
+	cheating = cheating_check(user_responses, user_input, word)
+	if cheating == bool(False):
+    	user_responses.append(user_input)
+	return get_result(word, user_input)[0], user_input
 
 
-def replace(word, letter, index):  # Méthode qui permet de remplacer un '_' dans un word par la letter à l'index + affiche le résultat
-    return ""
-
+def replace(word, letter, index, current_word):  # Méthode qui permet de remplacer un '_' dans un word par la letter à l'index + affiche le résultat
+	index_array=[0, 1]
+	index_array=index_array.append(index)
+	wordarray=list(word)
+	current_wordarray=list(current_word)
+	replacing=1
+	while len(index_array) <= replacing:
+    	replace_index = index[replacing]
+    	replace_letter = wordarray[replace_index]
+    	current_wordarray[replace_index]=replace_letter    
+    	current_word=current_word.join(current_wordarray)
+	replacing=replacing+1
+	return current_word
 
 def main():
-    draw = 0
-    user_responses = []
-    word = get_random_word()
-    length = len(word)
-    current_word = "_"
+	draw = 0
+	get_draw(draw)
+	user_responses = []
+	word = get_random_word()
+	length = len(word)
+	current_word = "_"
 
-    print(word)
+	print(word) #debug
 
-    while len(current_word) < length:
-        current_word += "_"
+	while len(current_word) < length:
+    	current_word += "_"
 
-    while current_word != word:
-        if draw == 6:
-            print("Vous avez perdu")
-            return
-        response = ask_for_a_letter(user_responses, word)
-        if response[0]:
-            print("Vous avez rentré la lettre " + response[1] + " et vous aviez raison !")
-            # replace(....)
-        else:
-            get_draw(draw)
-            print("La lettre " + response[1] + " est incorrecte, essayez encore.")
-
-
+	while current_word != word:
+    	if draw == 6:
+        	print("Vous avez perdu")
+        	return
+    	response = ask_for_a_letter(user_responses, word)
+    	if response[0]:
+        	print("Vous avez rentré la lettre " + response[1] + " et vous aviez raison !")
+        	# replace(....)
+    	else:
+        	get_draw(draw)
+        	print("La lettre " + response[1] + " est incorrecte, essayez encore.")
+#replace("COUCOU", "O", (2, 5), "C_U__U")
 main()
